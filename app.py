9e0285c74f7171b3de146cc9299021b9ddf69b81
raudhatulhanna <hannaraudhatul12@gmail.com>
2026-05-23 09:36:53 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py — baris 13
+st.set_page_config(
+  page_title="Finance Dashboard",
+  layout="wide"
+)
+
+# Hirarki teks
+st.title("📊 Dashboard")
+st.header("Laporan Bulanan")
+st.subheader("📈 Monthly Expenses")
+st.caption("Made with ❤️ using Streamlit")
